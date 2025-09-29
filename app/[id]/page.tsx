e83d1bb9c86c2f2dd5290bd7973c22565e09046a
Shakthi Mahendran <shakthimahendran2016@gmail.com>
2025-09-29 23:12:21 +0530

    changes made final

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -401,7 +401,7 @@ export default async function MainPage({
   async function getDomainDetails(domainName: string) {
     try {
       const res = await fetch(
-        `http://localhost:3000/api/domain?domain=${domainName}`
+        `https://kt.ignitte.org/api/domain?domain=${domainName}`
       );
 
       if (!res.ok) {
